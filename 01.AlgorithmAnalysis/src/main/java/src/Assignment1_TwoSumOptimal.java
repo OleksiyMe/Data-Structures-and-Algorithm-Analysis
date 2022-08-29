69493a487fc3deaf06419652b73aaf425ddf52b5
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-29 17:08:45 -0400

    refining of Umran Method for twoSumOptimalSolution

--- 01.AlgorithmAnalysis/src/main/java/src/Assignment1_TwoSumOptimal.java
+++ 01.AlgorithmAnalysis/src/main/java/src/Assignment1_TwoSumOptimal.java
@@ -1,24 +1,50 @@
 package src;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Assignment1_TwoSumOptimal {
     public static void main(String[] args) {
-        int[] array=new int[]{2,7,9,11};
-        System.out.println(Arrays.toString(twoSumOptimalSolution(array, 9)));
+        int[] array = new int[]{20, 2, 23, 7, 9, 11};
+        System.out.println(Arrays.toString(twoSumOptimalSolutionUmran(array, 9)));
     }
-    public static int[] twoSumOptimalSolution(int[] array, int targetValue){
+
+    public static int[] twoSumOptimalSolution(int[] array, int targetValue) {
         // Complexity of the solution is O(n) but I have space complexity of O(n)
-      // create hashmap
-        Map<Integer,Integer> map=new HashMap<>();
-        for (int i = 0; i <array.length ; i++) {
-            int potentialMatch=targetValue-array[i];
-            if (map.containsKey(potentialMatch)) return new int[] {i,map.get(potentialMatch)};
+        // create hashmap
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int potentialMatch = targetValue - array[i];
+            if (map.containsKey(potentialMatch)) return new int[]{i, map.get(potentialMatch)};
             else map.put(array[i], i);
 
         }
         return new int[]{};
     }
+
+    public static int[] twoSumOptimalSolutionUmran(int[] nums, int target) {  // O(n)
+
+        int[] indexes = new int[2];
+        ArrayList<Integer> tmp = new ArrayList<>();
+
+        for (int num : nums) {   //copy unsorted input array to tmp ArrayList
+            tmp.add(num);
+        }
+        Arrays.sort(nums);     // input array is sorted
+
+        for (int i = 0, j = 1; i < nums.length; i++, j++) {
+            if (nums[i] > target) continue;          // target lover than current
+            if (target - nums[i] == nums[j]) {                  // getting the indexes for sorted array
+                indexes[0] = i;
+                indexes[1] = j;
+                break;
+            }
+        }
+        indexes[0] = tmp.indexOf(nums[indexes[0]]);      //finding indexes for unsorted input
+        indexes[1] = tmp.indexOf(nums[indexes[1]]);
+        return indexes;
+    }
+
 }
