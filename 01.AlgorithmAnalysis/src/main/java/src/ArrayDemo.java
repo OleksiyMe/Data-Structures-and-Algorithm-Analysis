fa9bb021d09b1514d454dfb442ab95397a68bb76
Oleksiy Melnychuk <melnichuk@gmail.com>
2022-08-29 16:19:28 -0400

    let's do it this big commit

---
+++ 01.AlgorithmAnalysis/src/main/java/src/ArrayDemo.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] scoreArray=new int[10];
+
+    }
+}
